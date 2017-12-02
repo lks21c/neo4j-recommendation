77176b304d81ea12c39d2193996fc7f645a0024d
Kwangsik Lee <lks21c@gmail.com>
2017-12-02 20:02:08 +0900

    yes

--- recommend.py
+++ recommend.py
@@ -11,11 +11,11 @@ total = 943
 topK = 3
 
 query = (
-        'MATCH (b:User)-[r:RATED]->(m:Movie), (b)-[s:SIMILARITY]-(a:User {user_id:{user_id1}}) '
-        ' WHERE NOT((a)-[:RATED]->(m)) '
+        'MATCH (b:User)-[r:Has_rated]->(m:Movie), (b)-[s:SIMILARITY]-(a:User {user_id:{user_id1}}) '
+        ' WHERE NOT((a)-[:Has_rated]->(m)) '
         ' WITH m, s.similarity AS similarity, r.rating AS rating '
-        ' ORDER BY m.name, similarity DESC '
-        ' WITH m.name AS movie, COLLECT(rating)[0..{topK}] AS ratings '
+        ' ORDER BY m.title, similarity DESC '
+        ' WITH m.title AS movie, COLLECT(rating)[0..{topK}] AS ratings '
         ' WITH movie, REDUCE(s = 0, i IN ratings | s + i)*1.0 / LENGTH(ratings) AS reco '
         ' ORDER BY reco DESC '
         ' RETURN movie AS Movie, reco AS Recommendation '
@@ -24,3 +24,4 @@ query = (
 tx = graph.cypher.begin()
 tx.append(query, {'user_id1': 1, 'topK': topK})
 result = tx.commit()
+print result
