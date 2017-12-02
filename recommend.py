4e67102504976aa62b7cae842a601ddc01a5b2dd
Kwangsik Lee <lks21c@gmail.com>
2017-12-02 19:57:25 +0900

    yes

---
+++ recommend.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from py2neo import Graph
+
+graph = Graph('http://neo4j:melon123!@localhost:7474/db/data/')
+
+total = 943
+topK = 3
+
+query = (
+        'MATCH (b:User)-[r:RATED]->(m:Movie), (b)-[s:SIMILARITY]-(a:User {user_id:{user_id1}}) '
+        ' WHERE NOT((a)-[:RATED]->(m)) '
+        ' WITH m, s.similarity AS similarity, r.rating AS rating '
+        ' ORDER BY m.name, similarity DESC '
+        ' WITH m.name AS movie, COLLECT(rating)[0..{topK}] AS ratings '
+        ' WITH movie, REDUCE(s = 0, i IN ratings | s + i)*1.0 / LENGTH(ratings) AS reco '
+        ' ORDER BY reco DESC '
+        ' RETURN movie AS Movie, reco AS Recommendation '
+        )
+
+tx = graph.cypher.begin()
+tx.append(query, {'user_id1': 1, 'topK': topK})
+result = tx.commit()
